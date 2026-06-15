2f8bc1907292955babdbaf14ae710df528f7becb
luiz-igreen <luizsilvaluizsilva052@gmail.com>
2026-06-14 23:33:14 -0300

    Enhance cargo form with CBO options and validation
    Refactor cargo management form and add CBO dictionary

--- pages/cargos.py
+++ pages/cargos.py
@@ -2,50 +2,70 @@ import streamlit as st
 import pandas as pd
 from sqlalchemy import text
 
+# Dicionário padrão de Cargos da Construção Civil e seus CBOs (Ministério do Trabalho)
+CARGOS_CBO_MTE = {
+    "ARMADOR DE ESTRUTURA DE CONCRETO": "7153-15",
+    "APONTADOR DE OBRAS": "4122-05",
+    "CARPINTEIRO": "7155-05",
+    "ELETRICISTA DE INSTALAÇÕES": "7156-15",
+    "ENCANADOR": "7110-05",
+    "ENGENHEIRO CIVIL": "2142-05",
+    "MESTRE DE OBRAS": "7102-05",
+    "PEDREIRO": "7152-10",
+    "PINTOR DE OBRAS": "7166-10",
+    "SERVENTE DE OBRAS": "7170-20",
+    "SOLDADOR": "7243-15",
+    "TÉCNICO EM SEGURANÇA DO TRABALHO": "3516-05",
+    "VIGIA": "5174-20"
+}
+
+def limpar_formulario():
+    st.session_state.cargo_modo = 'novo'
+    st.session_state.cargo_codigo = ""
+    st.session_state.cargo_nome = list(CARGOS_CBO_MTE.keys())[0]
+    st.session_state.cargo_cbo = CARGOS_CBO_MTE[list(CARGOS_CBO_MTE.keys())[0]]
+
 def render(engine, *args, **kwargs):
     st.title("Gestão de Cargos e CBO")
-    st.markdown("Cadastro e consulta de cargos padronizados (Referência: Domínio Sistemas).")
+    st.markdown("Cadastro e consulta de cargos padronizados (Restrito à lista oficial do MTE).")
 
-    # 1. Inicialização das variáveis de estado (Correção do erro de carregamento)
+    # Inicialização de variáveis de estado
     if 'cargo_modo' not in st.session_state:
-        st.session_state.cargo_modo = 'novo'
-    if 'cargo_codigo' not in st.session_state:
-        st.session_state.cargo_codigo = ''
-    if 'cargo_nome' not in st.session_state:
-        st.session_state.cargo_nome = ''
-    if 'cargo_cbo' not in st.session_state:
-        st.session_state.cargo_cbo = ''
+        limpar_formulario()
 
-    def limpar_formulario():
-        st.session_state.cargo_modo = 'novo'
-        st.session_state.cargo_codigo = ''
-        st.session_state.cargo_nome = ''
-        st.session_state.cargo_cbo = ''
-
-    # 2. Layout Principal
     col_form, col_botoes = st.columns([3, 1])
 
     with col_form:
         st.subheader("Dados do Cargo")
 
-        # Campos de entrada controlados pelo estado
         codigo_input = st.text_input(
             "Código:", 
             value=st.session_state.cargo_codigo, 
             disabled=(st.session_state.cargo_modo == 'editando')
         )
 
-        nome_input = st.text_input(
-            "Nome:", 
-            value=st.session_state.cargo_nome,
-            disabled=(st.session_state.cargo_modo == 'consultando')
-        )
+        # Se estiver consultando, mostra o valor salvo. Se for novo/editando, mostra a lista restrita.
+        if st.session_state.cargo_modo == 'consultando':
+            nome_input = st.text_input("Nome:", value=st.session_state.cargo_nome, disabled=True)
+            cbo_input = st.text_input("C.B.O. 2002:", value=st.session_state.cargo_cbo, disabled=True)
+        else:
+            # Lista suspensa restrita aos cargos do dicionário
+            opcoes_cargos = list(CARGOS_CBO_MTE.keys())
 
-        cbo_input = st.text_input(
-            "C.B.O. 2002:", 
-            value=st.session_state.cargo_cbo,
-            disabled=(st.session_state.cargo_modo == 'consultando')
-        )
+            # Tenta definir o index baseado no cargo atual, se existir na lista
+            index_padrao = 0
+            if st.session_state.cargo_nome in opcoes_cargos:
+                index_padrao = opcoes_cargos.index(st.session_state.cargo_nome)
+
+            nome_input = st.selectbox(
+                "Nome (Selecione na lista oficial):", 
+                options=opcoes_cargos,
+                index=index_padrao
+            )
+
+            # Preenchimento automático do CBO baseado na seleção
+            cbo_automatico = CARGOS_CBO_MTE.get(nome_input, "")
+            cbo_input = st.text_input("C.B.O. 2002 (Automático):", value=cbo_automatico, disabled=True)
 
     with col_botoes:
         st.subheader("Ações")
@@ -101,7 +121,7 @@ def render(engine, *args, **kwargs):
 
                         conn.execute(query, {
                             "codigo": str(codigo_input),
-                            "nome": nome_input.upper(),
+                            "nome": nome_input,
                             "cbo": cbo_input
                         })
 
